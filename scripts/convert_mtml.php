f92fb78d88ada89e5187508083a0404728c45bb6
a.shigeru <a.shigeru@gmail.com>
2014-04-07 10:52:57 +0900

    リファクタ

--- scripts/convert_mtml.php
+++ scripts/convert_mtml.php
@@ -13,12 +13,12 @@ function file_get_html_pretty($path)
     $dom = file_get_html(STATIC_DIR . $path, false, null, -1, -1, true, true, DEFAULT_TARGET_CHARSET, false, DEFAULT_BR_TEXT);
 
     // 共通処理
-    if ($dom->getElementByTagName('html')) {
-        $dom->getElementByTagName('html')->lang = '<$mt:BlogLanguage$>';
+    if ($html = $dom->find('html', 0)) {
+        $html->lang = '<$mt:BlogLanguage$>';
     }
 
-    if ($dom->find('meta[charset]', 0)) {
-        $dom->find('meta[charset]', 0)->charset = '<$mt:PublishCharset$>';
+    if ($meta = $dom->find('meta[charset]', 0)) {
+        $meta->charset = '<$mt:PublishCharset$>';
     }
 
     return $dom;
@@ -35,7 +35,7 @@ function add_modifier($dom)
     ));
 }
 
-function output_file($path, $dom)
+function ssiToMTInclude($dom)
 {
     $ssi = array(
         '<!--#include virtual="/inc/header.html" -->'  => '<$mt:Include identifier="header"$>',
@@ -43,7 +43,12 @@ function output_file($path, $dom)
         '<!--#include virtual="/inc/sidebar.html" -->' => '<$mt:Include identifier="sidebar"$>',
     );
 
-    $dom = str_ireplace(array_keys($ssi), array_values($ssi), $dom);
+    return str_ireplace(array_keys($ssi), array_values($ssi), $dom);
+}
+
+function output_file($path, $dom)
+{
+    $dom = ssiToMTInclude($dom);
 
     return file_put_contents(TEMPLATE_DIR . $path, $dom);
 }
@@ -58,21 +63,21 @@ $dom = file_get_html_pretty("/inc/sidebar.html");
 output_file("/sidebar.mtml", $dom);
 
 $dom = file_get_html_pretty("/inc/header.html");
-$dom->getElementByTagName('h1')->innertext = '<$mt:BlogName encode_html="1"$>';
+$dom->find('h1', 0)->innertext = '<$mt:BlogName encode_html="1"$>';
 output_file("/header.mtml", $dom);
 
 /**
  * アーカイブテンプレート
  */
 $dom = file_get_html_pretty("/index.html");
-$dom->find('article h1', 0)->innertext  = '<$mt:EntryTitle$>';
-$dom->find('article p',  0)->innertext  = '<$mt:EntryExcerpt$>';
-$dom->find('article a[href]',  0)->href = '<$mt:EntryPermalink encode_html=\'1\'$>';
 
 $article = $dom->find('article',  0);
+$article->find('h1', 0)->innertext  = '<$mt:EntryTitle$>';
+$article->find('p',  0)->innertext  = '<$mt:EntryExcerpt$>';
+$article->find('a[href]',  0)->href = '<$mt:EntryPermalink encode_html=\'1\'$>';
 output_file("/entry_summary.mtml", $article);
 
-$dom->getElementByTagName('title')->innertext    = '<$mt:BlogName encode_html="1"$>';
+$dom->find('title', 0)->innertext    = '<$mt:BlogName encode_html="1"$>';
 $dom->find('meta[name=description]', 0)->content = '<$mt:BlogDescription$>';
 $dom->find('div.contents', 0)->innertext = <<<EOT
 <mt:Entries>
@@ -83,7 +88,7 @@ $dom = add_modifier($dom);
 output_file("/main_index.mtml", $dom);
 
 $dom = file_get_html_pretty("/category/index.html");
-$dom->getElementByTagName('title')->innertext    = '<$mt:CategoryLabel encode_html="1"$> | <$mt:BlogName encode_html="1"$>';
+$dom->find('title', 0)->innertext = '<$mt:CategoryLabel encode_html="1"$> | <$mt:BlogName encode_html="1"$>';
 $dom->find('meta[name=description]', 0)->content = '<$mt:CategoryDescription$>';
 $dom->find('div.contents', 0)->innertext = <<<EOT
 <mt:Entries>
@@ -97,8 +102,8 @@ output_file("/category_entry_listing.mtml", $dom);
  * 記事テンプレート
  */
 $dom = file_get_html_pretty("/category/pages.html");
-$dom->getElementByTagName('title')->innertext = '<$mt:EntryTitle encode_html="1"$> | <$mt:BlogName encode_html="1"$>';
-$dom->getElementByTagName('h1')->innertext    = '<$mt:EntryTitle encode_html="1"$>';
+$dom->find('title', 0)->innertext = '<$mt:EntryTitle encode_html="1"$> | <$mt:BlogName encode_html="1"$>';
+$dom->find('h1', 0)->innertext    = '<$mt:EntryTitle encode_html="1"$>';
 $dom->find('meta[name=description]', 0)->content = '<$mt:EntryExcerpt$>';
 $dom->find('meta[name=keywords]',    0)->content = '<$mt:EntryKeywords$>';
 $dom->find('div.entry-body', 0)->innertext = '<$mt:EntryBody$>';
@@ -106,8 +111,8 @@ $dom = add_modifier($dom);
 output_file("/entry.mtml", $dom);
 
 $dom = file_get_html_pretty("/pages/page1.html");
-$dom->getElementByTagName('title')->innertext = '<$mt:EntryTitle encode_html="1"$> | <$mt:BlogName encode_html="1"$>';
-$dom->getElementByTagName('h1')->innertext    = '<$mt:EntryTitle encode_html="1"$>';
+$dom->find('title', 0)->innertext = '<$mt:EntryTitle encode_html="1"$> | <$mt:BlogName encode_html="1"$>';
+$dom->find('h1',    0)->innertext = '<$mt:EntryTitle encode_html="1"$>';
 $dom->find('meta[name=description]', 0)->content = '<$mt:MTPageExcerpt$>';
 $dom->find('meta[name=keywords]',    0)->content = '<$mt:PageKeywords$>';
 $dom->find('div.entry-body', 0)->innertext = '<$mt:EntryBody$>';
